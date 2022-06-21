aca6af75833040c7279ad51f25bf34c7939cc675
Maryam Zahin <maryam.zahin@betpawa.com>
2022-06-21 09:47:04 +0300

    first commit

---
+++ src/main/java/com/example/springbootfirstwebapp/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.springbootfirstwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+//	@ResponseBody
+	public String loginMsg(ModelMap model)
+	{
+		//model.put("name", name);
+		
+		return "login";
+	}
+  }
+
